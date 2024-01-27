d20948cc8b28819fb3353357786e69805690f0c4
EmbugMaker <1019462035@qq.com>
2024-01-27 23:16:28 +0800

    Add STM32F091RCT6 DEMO

---
+++ STM32F091RCT6_MIN_SYS_BOARD/GPIO_EXTI/MDK-ARM/RTE/_GPIO_EXTI/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'GPIO_EXTI' 
+ * Target:  'GPIO_EXTI' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f0xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
